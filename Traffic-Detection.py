eba6b179cb0c41c579285d4fd3dbeb267b585eda
WildCard13 <56112929+WildCard13@users.noreply.github.com>
2020-05-31 21:42:13 +0530

    Add files via upload

---
+++ Traffic-Detection.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+net = cv2.dnn.readNet("yolov3.weights","yolov3.cfg")
+classes = []
+with open("yolov3.names","r") as f:
+     classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+
+img = cv2.imread("Image_1.jpg")
+img = cv2.resize(img,None,fx=0.6,fy=0.6)
+height,width,channels = img.shape
+
+
+blob = cv2.dnn.blobFromImage(img,0.00392,(416,416),(0,0,0),True,crop=False)
+
+
+
+net.setInput(blob)
+outs = net.forward(output_layers)
+
+boxes = []
+confidences = []
+
+for out in outs:
+    for detection in out:
+        scores = detection[6:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.5:
+
+            center_x = int(detection[0] * width)
+            center_y = int(detection[1] * height)
+            w = int(detection[2] * width)
+            h = int(detection[3] * height)
+
+            x = int(center_x - w/2)
+            y = int(center_y - h/2)
+            boxes.append([x, y, w, h])
+            confidences.append(float(confidence))
+
+indexes = cv2.dnn.NMSBoxes(boxes,confidences,0.4,0.4)
+print(indexes)
+v = len(indexes)
+print("The number of vehicles are : ",v)
+for i in range(len(boxes)):
+    if i in indexes:
+       x,y,w,h = boxes[i]
+       cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+
+
+cv2.imshow("Image",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
